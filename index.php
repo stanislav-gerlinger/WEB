ca16a2283c7ae954ed4c15145b1569529be436ab
stanislav gerlinger <stanislav.gerlinger@simbirsoft.com>
2019-02-17 12:09:43 +0400

    Project Init

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<link href="/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<header id="header"></header>
+		<main class="main">
+            <div class="container">
+                <div class="row">
+                    <div class="cell">
+                        <ul class="main-menu">
+                            <li>
+                                <a href="/mypage/">Моя страница</a>
+                            </li>
+                            <li><a href="/mypage/">Новости</a></li>
+                            <li><a href="/mypage/">Сообщения</a></li>
+                            <li><a href="/mypage/">Друзья</a></li>
+                        </ul>
+                    </div>
+                    <div class="cell main-content">
+                        <div>
+                            Что нового?
+                            <img class="vk" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/VK.com-logo.svg/768px-VK.com-logo.svg.png">
+                        </div>
+                        <div>
+                            <div>Истории<span style="float: right;">Настройки</span></div>
+                            <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</p>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                    <div class="cell">
+                        <ul class="second-menu">
+                            <li>
+                                <a href="/mypage/">Моя страница</a>
+                                <ul class="second-menu__submenu">
+                                    <li>
+                                        <a href="/mypage/">Моя страница</a>
+                                    </li>
+                                    <li><a href="/mypage/">Новости</a></li>
+                                    <li><a href="/mypage/">Сообщения</a></li>
+                                    <li><a href="/mypage/">Друзья</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/mypage/">Новости</a></li>
+                            <li><a href="/mypage/">Сообщения</a></li>
+                            <li><a href="/mypage/">Друзья</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </main>
+		<footer></footer>
+	</body>
+</html>
