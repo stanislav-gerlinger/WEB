d38612412f0c81b488133b1750a1de682abef555
HolderCube <kudryashovan2000@gmail.com>
2019-03-24 17:06:31 +0400

    Коммит

--- content/themes/skifcomputers/header.php
+++ content/themes/skifcomputers/header.php
@@ -23,33 +23,50 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<nav class="navbar navbar-default">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topnav" aria-expanded="false">
-								<span class="sr-only">Меню</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
-						<div class="collapse navbar-collapse" id="topnav">
-							<?php $args = array( 
-								'theme_location' => 'top',
-								'container'=> false,
-						  		'menu_id' => 'top-nav-ul',
-						  		'items_wrap' => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
-								'menu_class' => 'top-menu',
-						  		'walker' => new bootstrap_menu(true)		  		
-					  			);
-								wp_nav_menu($args);
-							?>
-						</div>
-					</nav>
+
+	<header class="user63447817 container-fluid">
+		<div class="user63447817 row">
+			<div class="user63447817 header-top">
+				<div class="user63447817 col-md-2">
+					<span class="user63447817 header_descr">WhatsApp Telegram</span>
 				</div>
+				<div class="user63447817 col-md-2 col-md-offset-8">
+					<a href="#" class="user63447817 registration">Зарегистрироваться / Войти</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="user63447817 row">
+			<div class="user63447817 col-md-6">
+				<nav class="user63447817 navbar">
+					<div class="user63447817 collapse navbar-collapse">
+						<?php $args = array( 
+							'theme_location' => 'top',
+							'container'=> false,
+					  		'menu_id' => 'top-nav-ul',
+					  		'items_wrap' => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
+							'menu_class' => 'top-menu',
+					  		'walker' => new bootstrap_menu(true)		  		
+				  			);
+							wp_nav_menu($args);
+							?>
+					</div>
+				</nav>
+			</div>
+			<div class="user63447817 col-md-3 col-md-offset-3">
+				<nav class="user63447817 navbar">
+					<div class="user63447817 collapse navbar-collapse">
+						<ul class="user63447817 nav navbar-nav">
+							<li><a href="#"><i class="user63447817 fa fa-instagram"></i></a></li>
+							<li><a href="#"><i class="user63447817 fa fa-pinterest-p"></i></a></li>
+							<li><a href="#"><i class="user63447817 fa fa-telegram"></i></a></li>
+							<li><a href="#"><i class="user63447817 fa fa-google-plus-official"></i></a></li>
+							<li><a href="#"><i class="user63447817 fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="user63447817 fa fa-vk"></i></a></li>
+						</ul>
+					</div>	
+				</nav>
+
 			</div>
 		</div>
 	</header>
